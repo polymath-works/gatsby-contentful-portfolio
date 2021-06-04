df7e749123222e08762dd1f0a97e76e9085eea6d
Andira <51116209+PolymathWorks@users.noreply.github.com>
2021-06-04 11:35:34 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,31 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    menu: [
+      { name: "Home", to: "/" },
+      { name: "About", to: "/about" },
+    ],
+    links: {
+      facebook: "https://www.facebook.com/",
+      instagram: "https://www.instagram.com/",
+      pinterest: "https://pinterest.com/",
+    },
+    locale: "en",
+    title: `John Doe`,
+    description: `Photography portfolio of John Doe`,
+    author: `@johndoe`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: CONTENTFUL_SPACE_ID,
+        accessToken: CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -21,27 +39,22 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: "gatsby-plugin-mailchimp",
       options: {
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        endpoint: process.env.MAILCHIMP_ENDPOINT,
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `John Doe`,
+        short_name: `johndoe`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#3182ce`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
